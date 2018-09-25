8c460884b700c22c675d87c316ca47588a92a5f3
Michael Hosmar <mhosmar@clearpathrobotics.com>
2018-09-25 17:31:23 -0400

    Fix install targets when Spinnaker is installed locally. Tabs in FindSpinnaker.

--- spinnaker_camera_driver/cmake/modules/FindSpinnaker.cmake
+++ spinnaker_camera_driver/cmake/modules/FindSpinnaker.cmake
@@ -4,14 +4,16 @@ unset(Spinnaker_LIBRARIES)
 
 find_path(Spinnaker_INCLUDE_DIRS NAMES
   Spinnaker.h
-  HINTS
+  PATHS
   /usr/include/spinnaker/
-  /usr/local/include/spinnaker/)
+  /usr/local/include/spinnaker/
+)
 
 find_library(Spinnaker_LIBRARIES NAMES Spinnaker
-    HINTS
-    /usr/lib
-    /usr/local/lib)
+  PATHS
+  /usr/lib
+  /usr/local/lib
+)
 
 if (Spinnaker_INCLUDE_DIRS AND Spinnaker_LIBRARIES)
   set(Spinnaker_FOUND 1)
