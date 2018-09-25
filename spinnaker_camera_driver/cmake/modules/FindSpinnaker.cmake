7562b63067f6a20933dd1233d122de07cddd33ec
Helen Oleynikova <helenoleynikova@gmail.com>
2018-09-25 23:26:25 +0200

    Add missing target (Cm3) and switch to find_package script. (#11)
    * Add missing target (Cm3) and switch to find_package script.
    
    * Clean up message.

---
+++ spinnaker_camera_driver/cmake/modules/FindSpinnaker.cmake
@@ -0,0 +1,18 @@
+unset(Spinnaker_FOUND)
+unset(Spinnaker_INCLUDE_DIRS)
+unset(Spinnaker_LIBRARIES)
+
+find_path(Spinnaker_INCLUDE_DIRS NAMES
+  Spinnaker.h
+  HINTS
+  /usr/include/spinnaker/
+  /usr/local/include/spinnaker/)
+
+find_library(Spinnaker_LIBRARIES NAMES Spinnaker
+    HINTS
+    /usr/lib
+    /usr/local/lib)
+
+if (Spinnaker_INCLUDE_DIRS AND Spinnaker_LIBRARIES)
+  set(Spinnaker_FOUND 1)
+endif (Spinnaker_INCLUDE_DIRS AND Spinnaker_LIBRARIES)
