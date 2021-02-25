13b15c9d906785969c1fbcdc652617bcda458c5d
Stephan Wirth <1481786+stwirth@users.noreply.github.com>
2021-02-25 11:32:54 +0100

    Make download work, use find_path and find_library afterwards

--- spinnaker_camera_driver/cmake/DownloadSpinnaker.cmake
+++ spinnaker_camera_driver/cmake/DownloadSpinnaker.cmake
@@ -9,13 +9,24 @@ function(download_spinnaker FLIR_LIB_VAR FLIR_INCLUDE_DIR_VAR)
     OUTPUT_STRIP_TRAILING_WHITESPACE)
 
   include(cmake/TargetArch.cmake)
-  target_architecture(FLIR_ARCH)
-  set(FLIR_DIR ${CMAKE_CURRENT_BINARY_DIR}/usr/lib)
+  target_architecture(TARGET_ARCH)
   set(DOWNLOAD_SCRIPT "${PROJECT_SOURCE_DIR}/cmake/download_spinnaker")
 
-  message(STATUS "Running download_spinnaker script with arguments: ${FLIR_ARCH} ${FLIR_DIR} ${OS_CODE_NAME}")
+  set(SPINNAKER_SDK_DIR ${CMAKE_CURRENT_BINARY_DIR}/spinnaker-sdk)
+
+  message(STATUS "Running download_spinnaker script with arguments: ${TARGET_ARCH} ${OS_CODE_NAME} ${SPINNAKER_SDK_DIR}")
   execute_process(
-    COMMAND ${DOWNLOAD_SCRIPT} ${FLIR_ARCH} "${FLIR_DIR}" ${OS_CODE_NAME})
-  set(${FLIR_LIB_VAR} "${FLIR_DIR}/libSpinnaker.so" PARENT_SCOPE)
-  set(${FLIR_INCLUDE_DIR_VAR} "${CMAKE_CURRENT_BINARY_DIR}/usr/include/spinnaker" PARENT_SCOPE)
+    COMMAND ${DOWNLOAD_SCRIPT} ${TARGET_ARCH} ${FLIR_DIR} ${OS_CODE_NAME} ${SPINNAKER_SDK_DIR}
+    RESULT_VARIABLE DOWNLOAD_EXIT_CODE)
+  if(NOT DOWNLOAD_EXIT_CODE EQUAL "0")
+    message(FATAL_ERROR "Could not download Spinnaker SDK!")
+  endif()
+
+  set(SPINNAKER_SEARCH_PATHS ${SPINNAKER_SDK_DIR}/usr ${SPINNAKER_SDK_DIR}/opt/spinnaker)
+
+  find_path(Spinnaker_INCLUDE_DIR NAMES Spinnaker.h PATHS ${SPINNAKER_SEARCH_PATHS} PATH_SUFFIXES include include/spinnaker)
+  find_library(Spinnaker_LIBRARIES NAMES Spinnaker PATHS ${SPINNAKER_SEARCH_PATHS} PATH_SUFFIXES lib)
+
+  set(${FLIR_INCLUDE_DIR_VAR} "${Spinnaker_INCLUDE_DIR}" PARENT_SCOPE)
+  set(${FLIR_LIB_VAR} "${Spinnaker_LIBRARIES}" PARENT_SCOPE)
 endfunction()
