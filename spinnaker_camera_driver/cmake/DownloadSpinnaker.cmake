e6265738d541fbcfb6f4d94604c7fc9e0598eea0
Luis Camero <lcamero@clearpathrobotics.com>
2022-03-22 15:46:19 -0400

    Spinnaker libraries are now all copied to usr/lib

--- spinnaker_camera_driver/cmake/DownloadSpinnaker.cmake
+++ spinnaker_camera_driver/cmake/DownloadSpinnaker.cmake
@@ -16,6 +16,6 @@ function(download_spinnaker FLIR_LIB_VAR FLIR_INCLUDE_DIR_VAR)
   message(STATUS "Running download_spinnaker script with arguments: ${FLIR_ARCH} ${FLIR_DIR} ${OS_CODE_NAME}")
   execute_process(
     COMMAND ${DOWNLOAD_SCRIPT} ${FLIR_ARCH} "${FLIR_DIR}" ${OS_CODE_NAME})
-  set(${FLIR_LIB_VAR} "${CMAKE_BINARY_DIR}/opt/spinnaker/lib/libSpinnaker.so" PARENT_SCOPE)
+  set(${FLIR_LIB_VAR} "${CMAKE_BINARY_DIR}/usr/lib/libSpinnaker.so" PARENT_SCOPE)
   set(${FLIR_INCLUDE_DIR_VAR} "${CMAKE_BINARY_DIR}/opt/spinnaker/include/" PARENT_SCOPE)
 endfunction()
