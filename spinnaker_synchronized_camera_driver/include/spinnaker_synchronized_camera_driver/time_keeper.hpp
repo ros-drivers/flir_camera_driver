43bb45ce8ffd8706d490dc823e99fb29ad25f583
Bernd Pfrommer <bernd.pfrommer@gmail.com>
2024-01-02 15:23:10 -0500

    added first implementation of synchronized driver

---
+++ spinnaker_synchronized_camera_driver/include/spinnaker_synchronized_camera_driver/time_keeper.hpp
@@ -0,0 +1,64 @@
+// -*-c++-*--------------------------------------------------------------------
+// Copyright 2023 Bernd Pfrommer <bernd.pfrommer@gmail.com>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef SPINNAKER_SYNCHRONIZED_CAMERA_DRIVER__TIME_KEEPER_HPP_
+#define SPINNAKER_SYNCHRONIZED_CAMERA_DRIVER__TIME_KEEPER_HPP_
+
+#include <memory>
+#include <rclcpp/rclcpp.hpp>
+#include <spinnaker_camera_driver/synchronizer.hpp>
+
+namespace spinnaker_synchronized_camera_driver
+{
+class SynchronizedCameraDriver;  // forward decl
+class TimeKeeper : public spinnaker_camera_driver::Synchronizer
+{
+public:
+  explicit TimeKeeper(size_t idx, const std::string & name, SynchronizedCameraDriver * driver)
+  : index_(idx), name_(name), driver_(driver)
+  {
+  }
+  const std::string & getName() const { return (name_); }
+
+  bool getTimeStamp(
+    uint64_t hostTime, uint64_t imageTime, uint64_t frameId, uint64_t * ft) override;
+
+  double getOffsetAverage() const
+  {
+    return (numOffset_ != 0 ? offsetSum_ / static_cast<double>(numOffset_) : 0);
+  }
+  double getOffsetVariance() const
+  {
+    // running variance is computed following this reference:
+    // https://www.johndcook.com/blog/standard_deviation/
+    return (numOffset_ > 1 ? S_ / static_cast<double>(numOffset_ - 1) : 0);
+  }
+  int64_t getNumFramesDropped() const { return (numFramesDropped_); }
+  void clearStatistics();
+
+private:
+  size_t index_;
+  std::string name_;
+  SynchronizedCameraDriver * driver_{nullptr};
+  uint64_t lastFrameId_{0};
+  uint64_t lastHostTime_{0};
+  int64_t numFramesDropped_{0};
+  size_t numOffset_{0};
+  double offsetSum_{0};
+  double S_{0};
+  double M_{0};
+};
+}  // namespace spinnaker_synchronized_camera_driver
+#endif  // SPINNAKER_SYNCHRONIZED_CAMERA_DRIVER__TIME_KEEPER_HPP_
---
+++ spinnaker_synchronized_camera_driver/src/time_estimator.cpp
@@ -0,0 +1,180 @@
+// -*-c++-*--------------------------------------------------------------------
+// Copyright 2023 Bernd Pfrommer <bernd.pfrommer@gmail.com>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <spinnaker_synchronized_camera_driver/logging.hpp>
+#include <spinnaker_synchronized_camera_driver/time_estimator.hpp>
+
+static rclcpp::Logger get_logger() { return (rclcpp::get_logger("cam_sync")); }
+
+namespace spinnaker_synchronized_camera_driver
+{
+void TimeEstimator::initialize(uint64_t t0, double dt)
+{
+  T0_ = t0;
+  x_[0] = 0;
+  x_[1] = dt;
+  const double processSigma = dt * 0.01;     // process noise
+  const double measurementSigma = dt * 0.1;  // measurement noise
+  Q_ = processSigma * processSigma;
+  // it is important to initialize the off-diagonal elements as well
+  // or else the
+  P_[0][0] = P_[1][1] = P_[0][1] = P_[1][0] = Q_;
+  R_ = measurementSigma * measurementSigma;
+  LOG_INFO_FMT("frequency estimator initialized with %.3f Hz", 1.0 / dt);
+  frameTimes_.push_back(FrameTime(0));
+}
+
+void TimeEstimator::updateKalman(double z)
+{
+  /*
+  The next synchronized frame time is the result of a
+  Kalman filter prediction step. When a new frame arrives that
+  no longer fits within the bounds of the current frame,
+  then the average (z) of the current frames' arrival times is
+  used as a measurement, which will ultimately and indirectly update
+  the estimated inter-frame time difference.
+  The state of the filter has thus two components:
+     x = [current_frame_arrival_time]
+         [frame_interval time       ]
+  where only x[0] is directly measured, but x[1] is not observed.
+
+  Filter equations:
+     x = [current_frame_arrival_time]
+         [frame_interval            ]
+     H = [1 0]
+     F = [1 1]
+         [0 1]
+     y = z - H * x = z - H * x[0]
+     S = H P H^T + R = P[0][0] + R
+     K = P H^T S^-1
+       = S^-1 * [P[0][0]]
+                [P[1][0]]
+     x = x + K y
+     P = (I - K H) * P
+    */
+  const double y = z - x_[0];      // residual
+  const double S = P_[0][0] + R_;  // measurement noise
+  const double S_inv = 1.0 / S;
+  x_[0] += S_inv * P_[0][0] * y;
+  x_[1] += S_inv * P_[1][0] * y;
+
+  const double f = 1 - S_inv * P_[0][0];
+  P_[0][0] *= f;  // this is what the filter equations reduce to
+  P_[0][1] *= f;  // due to H and K being very simple
+  P_[1][0] *= f;
+  P_[1][1] -= S_inv * P_[0][1] * P_[1][0];
+}
+
+int64_t TimeEstimator::predict()
+{
+  x_[0] += x_[1];
+  // x_[1] = x_[1];
+  P_[0][0] += Q_;
+  P_[1][1] += Q_;
+  P_[0][1] += Q_;
+  P_[1][0] += Q_;
+  return (static_cast<int64_t>(x_[0] * 1e9));
+}
+
+bool TimeEstimator::getTimeFromList(uint64_t t_a, uint64_t * T)
+{
+#if 0
+  LOG_INFO_FMT("checking list for time: %8ld", t_a);
+  for (const auto & tl : frameTimes_) {
+    LOG_INFO_FMT("   %8ld", tl.getFrameTime());
+  }
+#endif
+  constexpr size_t MAX_NUM_FRAMES_TO_KEEP = 5;
+  const auto t = static_cast<int64_t>(t_a);
+  const auto dT = static_cast<int64_t>(x_[1] * 1e9);
+  const auto T_min = (*frameTimes_.begin()).getFrameTime();
+  const int64_t dT_min = static_cast<int64_t>(T_min) - static_cast<int64_t>(t);
+
+  if (dT_min * 2 > dT) {
+    // far away from the oldest frame time
+    LOG_WARN("dropping very old frame!");
+    return (false);
+  }
+
+  if (dT_min >= 0 && dT_min * 2 <= dT) {
+    // this frame is older than the oldest frame time but still within range
+    (*frameTimes_.begin()).addTime(t);
+    *T = static_cast<uint64_t>(T_min) + T0_;
+    return (true);
+  }
+
+  // make sure to add new frames if needed
+  for (auto ft = *(frameTimes_.rbegin()); (t - ft.getFrameTime()) * 2 > dT;
+       ft = *(frameTimes_.rbegin())) {
+    if (ft.isValid()) {
+      updateKalman(ft.getAverageFrameTime());
+    }
+    const auto nextTime = predict();
+    frameTimes_.push_back(FrameTime(nextTime));
+  }
+
+  while (frameTimes_.size() >= MAX_NUM_FRAMES_TO_KEEP) {
+    frameTimes_.pop_front();
+  }
+
+  const int64_t T_max = (*frameTimes_.rbegin()).getFrameTime();
+  const auto dT_max = static_cast<int64_t>(t) - T_max;
+
+  if (dT_max > 0 && dT_max * 2 <= dT) {
+    // this frame is younger than the latest frame time but still in range
+    (*frameTimes_.rbegin()).addTime(t);
+    *T = static_cast<uint64_t>(T_max) + T0_;
+    return (true);
+  }
+
+  // the following search must succeed
+  for (auto it = frameTimes_.begin();; ++it) {
+    auto itp1 = it;
+    itp1++;
+    if (itp1 == frameTimes_.end()) {
+      break;
+    }
+    const auto t1 = it->getFrameTime();
+    const auto t2 = itp1->getFrameTime();
+    const auto dTi = t2 - t1;
+    if (t >= t1 && t < t2) {
+      if ((t - t1) * 2 < dTi) {
+        // closer to beginning of interval
+        (*it).addTime(t);
+        *T = static_cast<uint64_t>(t1) + T0_;
+        return (true);
+      } else {
+        // closer to end of interval
+        (*itp1).addTime(t);
+        *T = static_cast<uint64_t>(t2) + T0_;
+        return (true);
+      }
+    }
+  }
+  // since frames are added to frameTimes_ until the current
+  // time is included in the active frame list, the frame time
+  // should always be found in the list, and this point should
+  // never be reached.
+  LOG_ERROR("INTERNAL BUG, should never reach this point!!");
+  *T = static_cast<uint64_t>(T_max) + T0_;
+  return (true);
+}
+
+bool TimeEstimator::update(size_t idx, uint64_t t_a, uint64_t * frameTime)
+{
+  (void)idx;
+  return (getTimeFromList(t_a - T0_, frameTime));
+}
+}  // namespace spinnaker_synchronized_camera_driver
