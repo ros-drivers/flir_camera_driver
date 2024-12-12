8a1ce1438a5348486445e546d81c5e5c1fd285f6
tgiovenardi <tgiovenardi@gmail.com>
2024-12-12 15:23:09 -0300

    add: sm4 new config

--- spinnaker_camera_driver/launch/sm4_camera.launch.py
+++ spinnaker_camera_driver/launch/sm4_camera.launch.py
@@ -32,7 +32,7 @@ camera_params = {
     'gain_auto': 'Off',
     'gain': 0,
     'exposure_auto': 'Off',
-    'exposure_time': 8333.333,
+    'exposure_time': 3000,
     'line2_selector': 'Line2',
     'line2_v33enable': False,
     'line3_selector': 'Line3',
