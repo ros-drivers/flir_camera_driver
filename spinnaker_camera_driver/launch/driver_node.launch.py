73f8860cb9b1620ddba62fdd9a50d5a132a839e5
Bernd Pfrommer <bernd.pfrommer@gmail.com>
2026-08-01 19:57:39 -0400

    added exposure time limit parameters for blackfly_s

--- spinnaker_camera_driver/launch/driver_node.launch.py
+++ spinnaker_camera_driver/launch/driver_node.launch.py
@@ -173,7 +173,9 @@ def launch_setup(context, *args, **kwargs):
         parameters=[
             example_parameters[camera_type],
             {
-                'ffmpeg_image_transport.encoding': 'hevc_nvenc',
+                'flir_camera.image_raw.ffmpeg.encoder': 'hevc_nvenc',
+                'flir_camera.image_raw.ffmpeg.bit_rate': 10000000,
+                'flir_camera.image_raw.ffmpeg.encoder': 'libx264',
                 'parameter_file': parameter_file,
                 'serial_number': [LaunchConfig('serial')],
             },
